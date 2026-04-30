47e4933e6684f4d4693543a0d7e6b6ef0c6b0118
Débora Hernández <123646841+DeboraHernandez@users.noreply.github.com>
2026-04-30 11:54:32 -0600

    implement category and product page

--- frontend/src/app/category/page.tsx
+++ frontend/src/app/category/page.tsx
@@ -1,55 +1,195 @@
-
-import { products } from "@/data/products"
-import ProductCard from "@/components/ProductCard"
-
-type Props = {
-    params: {
-        slug: string
-    }
-    }
-
-export default function CategoryPage({ params }: Props) {
-    const filteredProducts = products.filter(
-        (product) => product.category === params.slug
-    )
-
-    return (
+ export default function Category() {
+  return (
+ 
+ <div>
         <div>
-        <div className="container grid">
-
-            {/* HERO LEFT */}
-
-            <div className="grid-wrapper-left">
-            <div className="relative sticky-up_80px">
-                <div
-                className="hero_baground flex-vertical-center"
-                style={{
-                    backgroundImage:
-                    "url(https://cdn.prod.website-files.com/6400d82951450087c6d1eba8/643432f353b60dfd4b4ecece_flowers.webp)",
-                }}
-                >
-                <div className="content">
-                    <h1 className="text-white z_index-1">
-                        {(Array.isArray(params?.slug) ? params.slug.join('/') : params?.slug ?? '').replace(/-/g, ' ')}
-                    </h1>
+            <div className="container grid">
+                <div id="w-node-a15c8879-0de5-f6e5-77df-73cbb1d029ae-f5f63cb6" className="grid-wrapper-left">
+                    <div className="relative sticky-up_80px">
+                        <div 
+                        style={{
+                            backgroundImage: 'url("https://cdn.prod.website-files.com/6400d82951450087c6d1eba8/643432f353b60dfd4b4ecece_flowers.webp")'
+                        }} 
+                        className="hero_baground flex-vertical-center"
+                        >
+                        <div className="content">
+                            <h1 className="text-white z_index-1">Fresh Flowers</h1>
+                        </div>
+                        </div>
+                        <div className="overlay_absolute"></div>
+                    </div>
                 </div>
+                <div id="w-node-a15c8879-0de5-f6e5-77df-73cbb1d029b5-f5f63cb6" className="grid-wrapper-right">
+                    <div className="w-dyn-list">
+                        <div role="list" className="grid-wrapper-copy w-dyn-items">
+                            <div data-w-id="a15c8879-0de5-f6e5-77df-73cbb1d029b8" role="listitem" className="w-dyn-item">
+                                <a href="/collection-product/bouquets-fresh-flowers-blue-harmony" className="wrapper_link-card_product w-inline-block">
+                                    <div className="card-product">
+                                        <div className="label">
+                                            <h6>Blue Harmony</h6>
+                                            <div className="display-flex_horizontal_center text-gray">
+                                                <p className="margin-right-4px caption">Price</p>
+                                                <p className="caption">$55</p>
+                                            </div>
+                                        </div>
+                                        <div className="card_img">
+                                            <img height="" loading="lazy" alt="" src="https://cdn.prod.website-files.com/6400d82951450087c6d1eba8/64342f30da060985be0af72e_640a0fe3f475267be455035a_DoubleLuna_Carouseljpg%2520copy%25202.webp" className="img_100x100-fit_cover"/>
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
+                            <div data-w-id="a15c8879-0de5-f6e5-77df-73cbb1d029b8" role="listitem" className="w-dyn-item">
+                                <a href="/collection-product/bouquets-fresh-flowers-rosy-delight" className="wrapper_link-card_product w-inline-block">
+                                    <div className="card-product">
+                                        <div className="label">
+                                            <h6>Rosy Delight</h6>
+                                            <div className="display-flex_horizontal_center text-gray">
+                                                <p className="margin-right-4px caption">Price</p>
+                                                <p className="caption">$120</p>
+                                            </div>
+                                        </div>
+                                        <div className="card_img">
+                                            <img height="" loading="lazy" alt="" src="https://cdn.prod.website-files.com/6400d82951450087c6d1eba8/64342f2fe557aa7e71a27760_640a0d9e587af69abd0e9f4c_rue-de-la-rosiere-en4%2520copy%25202.webp" sizes="(max-width: 479px) 100vw, (max-width: 991px) 50vw, 25vw" srcSet="https://cdn.prod.website-files.com/6400d82951450087c6d1eba8/64342f2fe557aa7e71a27760_640a0d9e587af69abd0e9f4c_rue-de-la-rosiere-en4%2520copy%25202-p-500.webp 500w, https://cdn.prod.website-files.com/6400d82951450087c6d1eba8/64342f2fe557aa7e71a27760_640a0d9e587af69abd0e9f4c_rue-de-la-rosiere-en4%2520copy%25202-p-800.webp 800w, https://cdn.prod.website-files.com/6400d82951450087c6d1eba8/64342f2fe557aa7e71a27760_640a0d9e587af69abd0e9f4c_rue-de-la-rosiere-en4%2520copy%25202-p-1080.webp 1080w, https://cdn.prod.website-files.com/6400d82951450087c6d1eba8/64342f2fe557aa7e71a27760_640a0d9e587af69abd0e9f4c_rue-de-la-rosiere-en4%2520copy%25202.webp 1200w" className="img_100x100-fit_cover"/>
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
+                            <div data-w-id="a15c8879-0de5-f6e5-77df-73cbb1d029b8" role="listitem" className="w-dyn-item">
+                                <a href="/collection-product/bouquets-fresh-flowers-dawns-delight" className="wrapper_link-card_product w-inline-block">
+                                    <div className="card-product">
+                                        <div className="label">
+                                            <h6>Dawn &#x27;s Delight</h6>
+                                            <div className="display-flex_horizontal_center text-gray">
+                                                <p className="margin-right-4px caption">Price</p>
+                                                <p className="caption">$100</p>
+                                            </div>
+                                        </div>
+                                        <div className="card_img">
+                                            <img height="" loading="lazy" alt="" src="https://cdn.prod.website-files.com/6400d82951450087c6d1eba8/64342f2f1d0047afe1dfaeff_640a0dd1910159199045fee7_point-du-jour%25202%2520copy%25202.webp" className="img_100x100-fit_cover"/>
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
+                            <div data-w-id="a15c8879-0de5-f6e5-77df-73cbb1d029b8" role="listitem" className="w-dyn-item">
+                                <a href="/collection-product/bouquets-fresh-flowers-serenity" className="wrapper_link-card_product w-inline-block">
+                                    <div className="card-product">
+                                        <div className="label">
+                                            <h6>Serenity</h6>
+                                            <div className="display-flex_horizontal_center text-gray">
+                                                <p className="margin-right-4px caption">Price</p>
+                                                <p className="caption">$89</p>
+                                            </div>
+                                        </div>
+                                        <div className="card_img">
+                                            <img height="" loading="lazy" alt="" src="https://cdn.prod.website-files.com/6400d82951450087c6d1eba8/64342f2f697cd605182dd107_640a036eec3dfd7a162d1fe9_championnet-en%2520copy%25202.webp" className="img_100x100-fit_cover"/>
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
+                            <div data-w-id="a15c8879-0de5-f6e5-77df-73cbb1d029b8" role="listitem" className="w-dyn-item">
+                                <a href="/collection-product/bouquets-fresh-flowers-boulevard-autumn-symphony" className="wrapper_link-card_product w-inline-block">
+                                    <div className="card-product">
+                                        <div className="label">
+                                            <h6>Autumn Symphony</h6>
+                                            <div className="display-flex_horizontal_center text-gray">
+                                                <p className="margin-right-4px caption">Price</p>
+                                                <p className="caption">$140</p>
+                                            </div>
+                                        </div>
+                                        <div className="card_img">
+                                            <img height="" loading="lazy" alt="" src="https://cdn.prod.website-files.com/6400d82951450087c6d1eba8/64342f2f6fe1f0d8327df0de_640a09cb9d7a288029c8e387_boulevard-saint-michel%2520copy.webp" className="img_100x100-fit_cover"/>
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
+                            <div data-w-id="a15c8879-0de5-f6e5-77df-73cbb1d029b8" role="listitem" className="w-dyn-item">
+                                <a href="/collection-product/bouquets-fresh-flowers-blanche" className="wrapper_link-card_product w-inline-block">
+                                    <div className="card-product">
+                                        <div className="label">
+                                            <h6>Snowfall</h6>
+                                            <div className="display-flex_horizontal_center text-gray">
+                                                <p className="margin-right-4px caption">Price</p>
+                                                <p className="caption">$70</p>
+                                            </div>
+                                        </div>
+                                        <div className="card_img">
+                                            <img height="" loading="lazy" alt="" src="https://cdn.prod.website-files.com/6400d82951450087c6d1eba8/64342f2e9c41bb0ea64e2eee_640a0f927a32f6e3e1771f01_%25D0%25B2%25D0%25B5%25D1%2580%25D1%2581%25D0%25B8%25D1%258F%25202%25202000reine-blanche.jpeg" sizes="(max-width: 479px) 100vw, (max-width: 991px) 50vw, 25vw" srcSet="https://cdn.prod.website-files.com/6400d82951450087c6d1eba8/64342f2e9c41bb0ea64e2eee_640a0f927a32f6e3e1771f01_%25D0%25B2%25D0%25B5%25D1%2580%25D1%2581%25D0%25B8%25D1%258F%25202%25202000reine-blanche-p-500.jpeg 500w, https://cdn.prod.website-files.com/6400d82951450087c6d1eba8/64342f2e9c41bb0ea64e2eee_640a0f927a32f6e3e1771f01_%25D0%25B2%25D0%25B5%25D1%2580%25D1%2581%25D0%25B8%25D1%258F%25202%25202000reine-blanche-p-800.jpeg 800w, https://cdn.prod.website-files.com/6400d82951450087c6d1eba8/64342f2e9c41bb0ea64e2eee_640a0f927a32f6e3e1771f01_%25D0%25B2%25D0%25B5%25D1%2580%25D1%2581%25D0%25B8%25D1%258F%25202%25202000reine-blanche-p-1080.jpeg 1080w, https://cdn.prod.website-files.com/6400d82951450087c6d1eba8/64342f2e9c41bb0ea64e2eee_640a0f927a32f6e3e1771f01_%25D0%25B2%25D0%25B5%25D1%2580%25D1%2581%25D0%25B8%25D1%258F%25202%25202000reine-blanche-p-1600.jpeg 1600w, https://cdn.prod.website-files.com/6400d82951450087c6d1eba8/64342f2e9c41bb0ea64e2eee_640a0f927a32f6e3e1771f01_%25D0%25B2%25D0%25B5%25D1%2580%25D1%2581%25D0%25B8%25D1%258F%25202%25202000reine-blanche-p-2000.jpeg 2000w, https://cdn.prod.website-files.com/6400d82951450087c6d1eba8/64342f2e9c41bb0ea64e2eee_640a0f927a32f6e3e1771f01_%25D0%25B2%25D0%25B5%25D1%2580%25D1%2581%25D0%25B8%25D1%258F%25202%25202000reine-blanche.jpeg 2000w" className="img_100x100-fit_cover"/>
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
+                            <div data-w-id="a15c8879-0de5-f6e5-77df-73cbb1d029b8" role="listitem" className="w-dyn-item">
+                                <a href="/collection-product/bouquets-fresh-flowers-rustic-charm" className="wrapper_link-card_product w-inline-block">
+                                    <div className="card-product">
+                                        <div className="label">
+                                            <h6>Rustic Charm</h6>
+                                            <div className="display-flex_horizontal_center text-gray">
+                                                <p className="margin-right-4px caption">Price</p>
+                                                <p className="caption">$90</p>
+                                            </div>
+                                        </div>
+                                        <div className="card_img">
+                                            <img height="" loading="lazy" alt="" src="https://cdn.prod.website-files.com/6400d82951450087c6d1eba8/64342f2e40bbcc5b78cafe5b_6409cda6c602fedec0e7f160_bocage%2520copy.webp" className="img_100x100-fit_cover"/>
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
+                            <div data-w-id="a15c8879-0de5-f6e5-77df-73cbb1d029b8" role="listitem" className="w-dyn-item">
+                                <a href="/collection-product/bouquets-fresh-flowers-pink-elegance" className="wrapper_link-card_product w-inline-block">
+                                    <div className="card-product">
+                                        <div className="label">
+                                            <h6>Pink Elegance</h6>
+                                            <div className="display-flex_horizontal_center text-gray">
+                                                <p className="margin-right-4px caption">Price</p>
+                                                <p className="caption">$70</p>
+                                            </div>
+                                        </div>
+                                        <div className="card_img">
+                                            <img height="" loading="lazy" alt="" src="https://cdn.prod.website-files.com/6400d82951450087c6d1eba8/64342f2ea49b494275934ff4_640a104b58678362abc066a7_beaux-arts%2520copy%25202.webp" className="img_100x100-fit_cover"/>
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
+                            <div data-w-id="a15c8879-0de5-f6e5-77df-73cbb1d029b8" role="listitem" className="w-dyn-item">
+                                <a href="/collection-product/bouquet-double-mystical-majesty" className="wrapper_link-card_product w-inline-block">
+                                    <div className="card-product">
+                                        <div className="label">
+                                            <h6>Mystical Majesty</h6>
+                                            <div className="display-flex_horizontal_center text-gray">
+                                                <p className="margin-right-4px caption">Price</p>
+                                                <p className="caption">$80</p>
+                                            </div>
+                                        </div>
+                                        <div className="card_img">
+                                            <img height="" loading="lazy" alt="" src="https://cdn.prod.website-files.com/6400d82951450087c6d1eba8/64342f2e3919510a13f29dc3_640a0db5f378af150d7c382c_Unicorn_Triple_Carousel%2520copy%25202.webp" className="img_100x100-fit_cover"/>
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
+                            <div data-w-id="a15c8879-0de5-f6e5-77df-73cbb1d029b8" role="listitem" className="w-dyn-item">
+                                <a href="/collection-product/bouquet-blazing-blossoms" className="wrapper_link-card_product w-inline-block">
+                                    <div className="card-product">
+                                        <div className="label">
+                                            <h6>Blazing Blossoms</h6>
+                                            <div className="display-flex_horizontal_center text-gray">
+                                                <p className="margin-right-4px caption">Price</p>
+                                                <p className="caption">$70</p>
+                                            </div>
+                                        </div>
+                                        <div className="card_img">
+                                            <img height="" loading="lazy" alt="" src="https://cdn.prod.website-files.com/6400d82951450087c6d1eba8/64342f2d9c41bb185f4e2ee7_640a0e53405d52850eb6f073_Firecracker_Triple_Carousel%2520copy%25202.webp" className="img_100x100-fit_cover"/>
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-
-                <div className="overlay_absolute"></div>
             </div>
-            </div>
-
-            {/* PRODUCTS RIGHT */}
-
-            <div className="grid-wrapper-right">
-            <div className="grid-wrapper-copy">
-                {filteredProducts.map((product) => (
-                <ProductCard key={product.id} product={product} />
-                ))}
-            </div>
-            </div>
-
         </div>
-        </div>
-    )
+        <script src="https://d3e54v103j8qbb.cloudfront.net/js/jquery-3.5.1.min.dc5e7f18c8.js?site=6400d82951450021c2d1eb7b" type="text/javascript" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossOrigin="anonymous"></script>
+        <script src="https://cdn.prod.website-files.com/6400d82951450021c2d1eb7b/js/webflow.b58961507.js" type="text/javascript"></script>
+    </div>
+
+      );
 }
