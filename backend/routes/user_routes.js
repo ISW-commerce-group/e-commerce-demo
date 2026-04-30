82765b7af59145f0c00d1e5d16aeae92a270e534
Débora Hernández <123646841+DeboraHernandez@users.noreply.github.com>
2026-04-29 18:58:57 -0600

    fix user_routes

--- backend/routes/user_routes.js
+++ backend/routes/user_routes.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const UserController = require('../controllers/userController');
+const UserController = require('../controllers/user_controller');
 
-router.get('/', UserController.getAllUsers);
-router.get('/:id', UserController.getUserById);
-router.get('/email/:email', UserController.getUserByEmail);
+router.get('/', UserController.get_all);
+router.get('/:id', UserController.get_by_id);
+router.get('/email/:email', UserController.get_by_email);
 
-router.post('/', UserController.createUser);
-router.put('/:id', UserController.updateUser);
-router.delete('/:id', UserController.deleteUser);
+router.post('/', UserController.create);
+router.put('/:id', UserController.update);
+router.delete('/:id', UserController.delete);
 
 module.exports = router;
